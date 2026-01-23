3a4f0318ebfb1f1e3d63f7956e192256e6b28343
Sizan Mahmud <sizanmahmud369@gmail.com>
2026-01-23 13:46:55 +0000

    এই সোশ্যাল বিজ্ঞাপনটি তুমি এমন ভাবে সেট করো যেন এটা সবসময় নিজের দিকে শো

--- src/components/GlobalAdScripts.tsx
+++ src/components/GlobalAdScripts.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-// This component is intentionally left blank to remove all global ad scripts.
+import Script from 'next/script';
+
 export function GlobalAdScripts() {
-    return null;
+    return (
+        <Script
+            id="social-bar-ad"
+            src="https://pl28457235.effectivegatecpm.com/8e/dd/54/8edd54854e77a6161245532c7f56ec4b.js"
+            strategy="afterInteractive"
+        />
+    );
 }
